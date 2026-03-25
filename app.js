eb927cd2e5e672d335989c8900d46e2296b61123
Elliot Datsomor <datsomorelliot56@gmail.com>
2026-03-25 10:09:02 +0000

    just to test something

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 
 const app = express();
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
